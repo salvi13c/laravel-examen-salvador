e28490964a52026709b04d7e12d2adf5a2a0d014
salvi <salolinu@gmail.com>
2021-02-16 20:37:10 +0100

    cambio de imagen de la pantalla principal en testing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
 @extends('layout')
 @section('content')
-<img src='https://images.pexels.com/photos/3837278/pexels-photo-3837278.jpeg?cs=srgb&dl=pexels-alexis-bahl-3837278.jpg&fm=jpg' width="250" height="250" alt='imagenhome'></
+<img src='https://images.pexels.com/photos/4709274/pexels-photo-4709274.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' width="250" height="320" alt='imagenhome'></
 @stop
